e788b9e3993b7a937cf068086e26ac2772f003d1
Elmin-Cr <elminc1@hotmail.com>
2025-10-22 14:45:38 +0200

    Most functions have been tested and converted to use with a vector.

--- DataManager.cpp
+++ DataManager.cpp
@@ -1,6 +1,8 @@
 #include "DataManager.h"
 #include <iostream>
 #include <ctime>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,41 +12,73 @@ using namespace std;
 void DataManager::Add_Value(double val, const std::string& tstamp) {
     Measure m;
     m.value = val;
-    m.timestamp = tstamp
+    m.timestamp = tstamp;
     data.push_back(m);
     
 }
 
-void DataManager::Show_Value(double val, const std::string& tstamp) {
-    for (size_t i = 0; i < data.size());
-    i++ {
-        cout << data[i].timestamp << " " << data[i].value
+void DataManager::Show_Value() {
+    if (data.empty()) {
+        cout << "Listan har inga värden" << '\n';
+    }
+
+    for (size_t i = 0; i < data.size(); i++)
+    {
+        cout << data[i].timestamp << " " << data[i].value << '\n';
     }
     
 }
 
-void DataManager::Calc_Sum(double val) {
-    
+void DataManager::Calc_Sum(double &sum) {
+    sum = 0.0;
+    if(data.empty())
+        return;
+    for (const auto &m : data) sum += m.value; 
 }
 
-void DataManager::Calc_Avg(double val) {
-
+void DataManager::Calc_Avg(double &avg) {
+    avg = 0.0;
+    if(data.empty())
+        return;
+    double sum;
+    Calc_Sum(sum);
+    avg = sum / data.size();
 }
 
 void DataManager::Calc_Min(double &MinVal) {
-
+    if(data.empty())
+        return;
+    MinVal = data.front().value;
+    for (const auto &m : data) if (m.value < MinVal) MinVal = m.value;
 }
 
 void DataManager::Calc_Max(double &MaxVal) {
-
+    if(data.empty())
+        return;
+    MaxVal = 0.0;
+    MaxVal = data.front().value;
+    for (const auto &m : data) if (m.value > MaxVal) MaxVal = m.value;
 }
 
 void DataManager::Calc_Var(double &var) {
-
+    if(data.empty())
+        return;
+    var = 0.0;
+    double avg;
+    Calc_Avg(avg);
+    for (const auto &m : data) {
+        var += (m.value - avg) * (m.value - avg);
+    }
+            var /= data.size();
 }
 
-void DataManager::calc_StdDev(double &stdav) {
-
+void DataManager::calc_StdDev(double &StDev) {
+    if(data.empty())
+        return;
+    StDev = 0.0;
+    double var;
+    Calc_Var(var);
+    StDev = sqrt(var);
 }
 
 void DataManager::Sort_Func_Asc(double &val) {
